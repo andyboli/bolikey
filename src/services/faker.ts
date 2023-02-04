08319663d7c20e075bfc8c742cd6e4bd266a70b2
andyboli <webdevelopmentbolivar@gmail.com>
2023-02-04 19:25:17 -0300

    create FakerService

---
+++ src/services/faker.ts
@@ -0,0 +1,36 @@
+import { faker } from "@faker-js/faker";
+
+const getRandomNumber = (min = 0) =>
+  faker.datatype.number({
+    precision: 1,
+    max: 9,
+    min,
+  });
+
+const getLessThanMinNumber = (minNumber: number) =>
+  minNumber -
+  faker.datatype.number({
+    precision: 1,
+    max: minNumber,
+    min: minNumber === 0 ? 0 : 1,
+  });
+
+const getGreaterThanMinNumber = (minNumber: number) =>
+  minNumber + faker.datatype.number({ precision: 1, max: 9, min: 1 });
+
+const getRandomWord = (length: number = 3) => faker.lorem.word({ length });
+
+const getRandomPassword = (
+  length = 3,
+  regex = /^[a-zA-Z0-9!@#$%^&*()\-+\\\/{}\[\]]*$/
+) => faker.internet.password(length, undefined, regex);
+
+const FakerServicer = {
+  getRandomNumber,
+  getRandomWord,
+  getRandomPassword,
+  getLessThanMinNumber,
+  getGreaterThanMinNumber,
+};
+
+export default FakerServicer;
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 import ApolloService from "./apollo";
 import DotenvService from "./dotenv";
 import ExpressService from "./express";
+import FakerService from "./faker";
 
-export default { ApolloService, DotenvService, ExpressService };
+export default { ApolloService, DotenvService, ExpressService, FakerService };
