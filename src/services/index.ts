d2f191622d63ed68322df449ad8163e5af888fc8
andyboli <webdevelopmentbolivar@gmail.com>
2023-02-04 12:48:06 -0300

    create AppService

---
+++ src/services/index.ts
@@ -0,0 +1,5 @@
+import ApolloService from "./apollo";
+import DotenvService from "./dotenv";
+import ExpressService from "./express";
+
+export default { ApolloService, DotenvService, ExpressService };
