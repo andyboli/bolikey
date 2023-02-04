78baea53e4e47e7b7ee036aa1442b44669345678
andyboli <webdevelopmentbolivar@gmail.com>
2023-02-04 12:48:45 -0300

    create controller test

---
+++ src/controller.test.ts
@@ -0,0 +1,17 @@
+import { faker } from "@faker-js/faker";
+
+import AppController from "./controller";
+
+describe("Test App Controller", () => {
+  it("minDigit", () => {});
+
+  it("minLowercase", () => {});
+
+  it("minSize", () => {});
+
+  it("minSpecialChars", () => {});
+
+  it("minUppercase", () => {});
+
+  it("noRepeted", () => {});
+});
