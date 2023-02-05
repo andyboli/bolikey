8ce5506f0cbdb6e26111cf7a6191fb5f167a0b2b
andyboli <webdevelopmentbolivar@gmail.com>
2023-02-05 12:32:31 -0300

    implementing verify resolver

--- src/services/apollo/resolvers.ts
+++ src/services/apollo/resolvers.ts
@@ -1,19 +1,44 @@
+import AppController from "../../controller";
+
 enum RuleTypes {
-  minDigit,
-  minLowercase,
-  minSize,
-  minSpecialChars,
-  minUppercase,
-  noRepeted,
+  "minDigit" = "minDigit",
+  "minLowercase" = "minLowercase",
+  "minSize" = "minSize",
+  "minSpecialChars" = "minSpecialChars",
+  "minUppercase" = "minUppercase",
+  "noRepeted" = "noRepeted",
 }
 
 type Rule = {
   rule: RuleTypes;
-  value: Number;
+  value: number;
 };
 
-const verify = (password: string, rules: Rule) => {
-  return { verify: false, noMatch: ["noRepeted"] };
+interface VerifyArgsInterface {
+  password: string;
+  rules: Rule[];
+}
+
+const mapRuleToController = {
+  [RuleTypes.minDigit]: AppController.minDigit,
+  [RuleTypes.minLowercase]: AppController.minLowercase,
+  [RuleTypes.minSize]: AppController.minSize,
+  [RuleTypes.minSpecialChars]: AppController.minSpecialChars,
+  [RuleTypes.minUppercase]: AppController.minUppercase,
+  [RuleTypes.noRepeted]: AppController.noRepeted,
+};
+
+const verify = (parent: any, { password, rules }: VerifyArgsInterface) => {
+  const noMatchRules = rules
+    .filter(({ rule, value }) => {
+      if (rule === RuleTypes.noRepeted)
+        return !mapRuleToController[rule](password);
+
+      return !mapRuleToController[rule]({ password, minNumber: value });
+    })
+    .map(({ rule }) => rule);
+
+  return { verify: !noMatchRules.length, noMatch: noMatchRules };
 };
 
 export const Query = { verify };
