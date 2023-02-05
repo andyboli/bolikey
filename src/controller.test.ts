55b0ddb98c995101378197aaee7d3a1adf4d2016
andyboli <webdevelopmentbolivar@gmail.com>
2023-02-05 09:36:52 -0300

    Test minLowercase function

--- src/controller.test.ts
+++ src/controller.test.ts
@@ -1,6 +1,9 @@
 import AppController from "./controller";
 import AppService from "./services";
 
+const TEXT_REGEX = /^[a-zA-Z]*$/;
+const NUMBER_REGEX = /^[0-9]*$/;
+
 const getRandomNumbers = (length: number) => {
   const randomNumbers = [];
 
@@ -17,6 +20,7 @@ describe("Test App Controller", () => {
       const minNumberMock = AppService.FakerService.getRandomNumber();
       const randomNumbers = getRandomNumbers(minNumberMock);
       const randomPassword = AppService.FakerService.getRandomPassword();
+
       const randomPasswordOnlyWithIntegersMock = randomNumbers.join("");
       const randomPasswordWithIntegersAtStartMock =
         randomPasswordOnlyWithIntegersMock + randomPassword;
@@ -26,6 +30,7 @@ describe("Test App Controller", () => {
         randomPassword + randomNumbers.join("") + randomPassword;
       const randomPasswordWithIntegersSpreadedAtMiddleMock =
         randomPassword + randomNumbers.join(randomPassword) + randomPassword;
+
       const validPasswordsMocks = [
         randomPasswordOnlyWithIntegersMock,
         randomPasswordWithIntegersAtStartMock,
@@ -45,7 +50,6 @@ describe("Test App Controller", () => {
     });
 
     it("Should return false when the password has less than the min number quantity of integers", () => {
-      const TEXT_REGEX = /^[a-zA-Z]*$/;
       const minNumberMock = AppService.FakerService.getRandomNumber(1);
       const lessThanMinNumberMock =
         AppService.FakerService.getLessThanMinNumber(minNumberMock);
@@ -54,6 +58,7 @@ describe("Test App Controller", () => {
         undefined,
         TEXT_REGEX
       );
+
       const randomPasswordOnlyWithIntegersMock = randomNumbers.join("");
       const randomPasswordWithIntegersAtStartMock =
         randomPasswordOnlyWithIntegersMock + randomTextPassword;
@@ -65,6 +70,7 @@ describe("Test App Controller", () => {
         randomTextPassword +
         randomNumbers.join(randomTextPassword) +
         randomTextPassword;
+
       const invalidPasswordsMocks = [
         randomPasswordOnlyWithIntegersMock,
         randomPasswordWithIntegersAtStartMock,
@@ -84,7 +90,94 @@ describe("Test App Controller", () => {
     });
   });
 
-  describe("Test minLowercase function ", () => {});
+  describe("Test minLowercase function ", () => {
+    it("Should return true when the password has at least the min number quantity of lowercase caracters", () => {
+      const minNumberMock = AppService.FakerService.getRandomNumber();
+      const randomPassword = AppService.FakerService.getRandomPassword();
+
+      const randomPasswordOnlyWithLowercaseCaracters =
+        AppService.FakerService.getRandomWord(
+          minNumberMock
+        ).toLocaleLowerCase();
+      const randomPasswordWithLowercaseCaractersAtStartMock =
+        randomPasswordOnlyWithLowercaseCaracters + randomPassword;
+      const randomPasswordWithLowercaseCaractersAtEndMock =
+        randomPassword + randomPasswordOnlyWithLowercaseCaracters;
+      const randomPasswordWithLowercaseCaractersAtMiddleMock =
+        randomPassword +
+        randomPasswordOnlyWithLowercaseCaracters +
+        randomPassword;
+      const randomPasswordWithLowercaseCaractersSpreadedAtMiddleMock =
+        randomPassword +
+        randomPasswordOnlyWithLowercaseCaracters
+          .split("")
+          .join(randomPassword) +
+        randomPassword;
+
+      const validPasswordsMocks = [
+        randomPasswordOnlyWithLowercaseCaracters,
+        randomPasswordWithLowercaseCaractersAtStartMock,
+        randomPasswordWithLowercaseCaractersAtEndMock,
+        randomPasswordWithLowercaseCaractersAtMiddleMock,
+        randomPasswordWithLowercaseCaractersSpreadedAtMiddleMock,
+      ];
+
+      validPasswordsMocks.forEach((validPasswordMock) =>
+        expect(
+          AppController.minLowercase({
+            password: validPasswordMock,
+            minNumber: minNumberMock,
+          })
+        ).toBeTruthy()
+      );
+    });
+
+    it("Should return false when the password has less than the min number quantity of lowercase caracters", () => {
+      const minNumberMock = AppService.FakerService.getRandomNumber(1);
+      const lessThanMinNumberMock =
+        AppService.FakerService.getLessThanMinNumber(minNumberMock);
+      const randomNumberPassword = AppService.FakerService.getRandomPassword(
+        undefined,
+        NUMBER_REGEX
+      );
+
+      const randomPasswordOnlyWithLowercaseCaracters =
+        AppService.FakerService.getRandomWord(
+          lessThanMinNumberMock
+        ).toLocaleLowerCase();
+      const randomPasswordWithLowercaseCaractersAtStartMock =
+        randomPasswordOnlyWithLowercaseCaracters + randomNumberPassword;
+      const randomPasswordWithLowercaseCaractersAtEndMock =
+        randomNumberPassword + randomPasswordOnlyWithLowercaseCaracters;
+      const randomPasswordWithLowercaseCaractersAtMiddleMock =
+        randomNumberPassword +
+        randomPasswordOnlyWithLowercaseCaracters +
+        randomNumberPassword;
+      const randomPasswordWithLowercaseCaractersSpreadedAtMiddleMock =
+        randomNumberPassword +
+        randomPasswordOnlyWithLowercaseCaracters
+          .split("")
+          .join(randomNumberPassword) +
+        randomNumberPassword;
+
+      const invalidPasswordsMocks = [
+        randomPasswordOnlyWithLowercaseCaracters,
+        randomPasswordWithLowercaseCaractersAtStartMock,
+        randomPasswordWithLowercaseCaractersAtEndMock,
+        randomPasswordWithLowercaseCaractersAtMiddleMock,
+        randomPasswordWithLowercaseCaractersSpreadedAtMiddleMock,
+      ];
+
+      invalidPasswordsMocks.forEach((invalidPasswordMock) =>
+        expect(
+          AppController.minLowercase({
+            password: invalidPasswordMock,
+            minNumber: minNumberMock,
+          })
+        ).toBeFalsy()
+      );
+    });
+  });
 
   describe("Test minSize function ", () => {});
 
